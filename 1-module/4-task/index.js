3d416084096478e80e5024296cc980aef8276d41
tarasBatya <tbatyan@mail.ru>
2021-07-29 13:54:28 +0300

    1-module> 4-task + 5-task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  
+  let lowerCase = str.toLowerCase();
+  
+  if (lowerCase.includes('1xbet') || 
+       lowerCase.includes('xxx')) {
+      return true;
+  }
+      return false;
 }
