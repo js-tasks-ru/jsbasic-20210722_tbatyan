64ce7d7dc0d5e7d85892f0e380295cf5e204c8d3
tarasBatya <tbatyan@mail.ru>
2021-09-03 17:20:32 +0300

    1-modulle-1-task

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,94 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    
+    this.render();
+    this.onClick_Arrows();
+    this.onClick_Ribbon_Tabs();
   }
-}
+
+  render () {
+    this.elem = createElement (`
+  <div class="ribbon">
+    <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+    <nav class="ribbon__inner"></nav>
+    <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+  </div>
+  `);
+
+    let categories_Links = "";
+
+      for (let i = 0; i < this.categories.length; i++) {
+        let name = this.categories[i].name;
+        let id = this.categories[i].id;
+  
+        categories_Links += `<a href="#" class="ribbon__item" data-id="${id}"> ${name} </a> \n`;
+      }; 
+
+      let ribbon__inner = this.elem.querySelector(".ribbon__inner");
+      ribbon__inner.innerHTML = categories_Links;
+  };
+
+  onClick_Arrows() {
+
+        let arrow_Right = this.elem.querySelector(".ribbon__arrow_right");
+        let arrow_Left = this.elem.querySelector(".ribbon__arrow_left");
+     
+        let ribbon_Inner = this.elem.querySelector(".ribbon__inner");
+      
+  arrow_Left.classList.remove("ribbon__arrow_visible");
+
+     arrow_Right.addEventListener('click', () => {
+        ribbon_Inner.scrollBy(350, 0);
+     });
+     
+     arrow_Left.addEventListener('click', () => {
+        ribbon_Inner.scrollBy(-350, 0);
+     });
+
+          ribbon_Inner.addEventListener('scroll', () => {
+
+              let scrollWidth = ribbon_Inner.scrollWidth;
+              let scrollLeft = ribbon_Inner.scrollLeft;
+              let clientWidth = ribbon_Inner.clientWidth;
+  
+              let scrollRight = scrollWidth - scrollLeft - clientWidth;
+
+            arrow_Left.classList.add("ribbon__arrow_visible");
+            arrow_Right.classList.add("ribbon__arrow_visible");
+
+              if (scrollRight < 1) {
+                    arrow_Right.classList.remove("ribbon__arrow_visible"); 
+              }
+
+              if (scrollLeft == 0) {          
+                    arrow_Left.classList.remove("ribbon__arrow_visible");
+              }
+          });
+  };
+
+  onClick_Ribbon_Tabs () {
+
+        let ribbon_Inner = this.elem.querySelector(".ribbon__inner");
+    
+    ribbon_Inner.addEventListener('click', (event) => {
+
+      event.preventDefault();
+      ribbon_Inner.classList.remove("ribbon__item_active");
+
+        let currentCategory = event.currentTarget;
+      
+      currentCategory.classList.add("ribbon__item_active");
+          
+              this.elem.dispatchEvent(new CustomEvent('product-add', {
+                  detail: currentCategory.id,
+                  bubbles: true
+              }));
+    });
+  };
+
+};
