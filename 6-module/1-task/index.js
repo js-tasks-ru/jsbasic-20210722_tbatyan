820e2dc712cb4cd34d6b8015a81c6b3bcbcc6e7b
tarasBatya <tbatyan@mail.ru>
2021-09-14 16:36:08 +0300

    6-module-1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,56 @@
  *      }
  *
  */
+
+ // import createElement from '../../assets/lib/create-element.js'; 
+ // doesn't work for td and some other elements that may not be placed into <div>
+
 export default class UserTable {
   constructor(rows) {
+      this.rows = rows;
+
+      this.render ();
   }
+
+render () {
+
+  this.elem = document.createElement('table');
+  
+  this.elem.innerHTML = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>`;
+  
+let newRows = "";
+
+for (let i= 0; i< this.rows.length; i++) {
+   let name = this.rows[i].name;
+   let age = this.rows[i].age;
+   let salary = this.rows.salary[i].salary;
+   let city = this.rows.city[i].city;
+  
+  newRows += `
+      <tr>
+        <td>${name}</td>
+        <td>${age}</td>
+        <td>${salary}</td>
+        <td>${city}</td>
+        <td><button>X</button></td>
+      </tr> \n`;
+    };
+  
+    let tbody = this.elem.querySelector('tbody');
+    tbody.innerHTML = newRows;
+
+  }
+
 }
